b0f123c582388b38f90b7341dba3360748f08d32
Diva Zahra Fakhirah <dvazahr31@gmail.com>
2025-05-04 18:24:41 +0700

    Membuat tampilan list

--- app/src/main/java/com/divazahra0070/asesmen2mobpro/MainActivity.kt
+++ app/src/main/java/com/divazahra0070/asesmen2mobpro/MainActivity.kt
@@ -5,8 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,7 +22,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.divazahra0070.asesmen2mobpro.model.Film
+import com.divazahra0070.asesmen2mobpro.screen.MainViewModel
 import com.divazahra0070.asesmen2mobpro.ui.theme.Asesmen2MobproTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,8 +65,47 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
 
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data) {
+            ListItem(film = it)
+            HorizontalDivider()
+        }
+    }
+}
 
+@Composable
+fun ListItem(film: Film) {
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = film.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = film.review,
+            maxLines = 4,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = film.genre,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = film.tanggal,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
 }
 
 @Preview(showBackground = true)
@@ -61,6 +113,6 @@ fun ScreenContent(modifier: Modifier = Modifier) {
 @Composable
 fun MainScreenPreview() {
     Asesmen2MobproTheme {
-    MainScreen()
+        MainScreen()
     }
 }
