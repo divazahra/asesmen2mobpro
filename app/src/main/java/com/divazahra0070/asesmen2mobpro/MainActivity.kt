e053412924ff4eab3db683ae0088c41b551085f0
Diva Zahra Fakhirah <dvazahr31@gmail.com>
2025-05-06 20:46:43 +0700

    Menyimpan preference pengguna

--- app/src/main/java/com/divazahra0070/asesmen2mobpro/MainActivity.kt
+++ app/src/main/java/com/divazahra0070/asesmen2mobpro/MainActivity.kt
@@ -36,9 +36,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -56,7 +53,11 @@ import com.divazahra0070.asesmen2mobpro.navigation.Screen
 import com.divazahra0070.asesmen2mobpro.navigation.SetupNavGraph
 import com.divazahra0070.asesmen2mobpro.screen.MainViewModel
 import com.divazahra0070.asesmen2mobpro.ui.theme.Asesmen2MobproTheme
+import com.divazahra0070.asesmen2mobpro.util.SettingsDataStore
 import com.divazahra0070.asesmen2mobpro.util.ViewModelFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -73,7 +74,8 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavHostController) {
-    var showList by remember { mutableStateOf(true) }
+    val dataStore = SettingsDataStore(LocalContext.current)
+    val showList by dataStore.layoutFlow.collectAsState(true)
 
     Scaffold(
         topBar = {
@@ -86,7 +88,11 @@ fun MainScreen(navController: NavHostController) {
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 actions = {
-                    IconButton(onClick = { showList = !showList }) {
+                    IconButton(onClick = {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            dataStore.saveLayout(!showList)
+                        }
+                    }) {
                         Icon(
                             painter = painterResource(
                                 if (showList) R.drawable.baseline_grid_view_24
