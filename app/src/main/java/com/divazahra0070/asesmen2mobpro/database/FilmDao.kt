040ee71dcc8707520ddb9222cd26a80dba348d4e
Diva Zahra Fakhirah <dvazahr31@gmail.com>
2025-05-06 15:32:36 +0700

    Membuat database dengan Room

---
+++ app/src/main/java/com/divazahra0070/asesmen2mobpro/database/FilmDao.kt
@@ -0,0 +1,21 @@
+package com.divazahra0070.asesmen2mobpro.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.divazahra0070.asesmen2mobpro.model.Film
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FilmDao {
+
+    @Insert
+    suspend fun insert(film: Film)
+
+    @Update
+    suspend fun update(film: Film)
+
+    @Query("SELECT * FROM film ORDER BY judul DESC")
+    fun getFilm(): Flow<List<Film>>
+}
